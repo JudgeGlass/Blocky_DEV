1d2cc546a14b9e7aa7a84830da11f6629e1a6695
Hunter Wilcox <hunter.wilcox@judgeglass.net>
2023-01-08 13:00:43 -0500

    removed old glsl

--- fragment.glsl
+++ fragment.glsl
@@ -1,6 +1,5 @@
 #version 330 core
 
-in vec3 fragmentColor;
 out vec4 color;
 
 in vec2 TexCoord;
--- vertex.glsl
+++ vertex.glsl
@@ -2,18 +2,14 @@
 
 
 layout(location = 0) in vec3 vertexPosition_modelspace;
-layout(location = 1) in vec3 vertexColor;
 layout(location = 2) in vec2 aTexCoord;
  
 uniform mat4 MVP;
 
-out vec3 fragmentColor;
 out vec2 TexCoord;
 
 void main(){
     gl_Position = MVP * vec4(vertexPosition_modelspace, 1.0f);  
-
-    fragmentColor = vertexColor;
 
     TexCoord = aTexCoord;
 }
